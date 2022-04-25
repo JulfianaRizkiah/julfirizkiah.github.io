6c69b0c0393eb80b9f5437fa428c1c89c01db9ec
Julfiana Rizkiah <julfirizkiah37@gmail.com>
2022-04-25 07:55:56 +0700

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="Footer">
+        <br></br>
+<hr></hr>
+
+<p>Copyright &copy; 2022 Julfiana Rizkiah F.</p>
+    
+ 
+
+    </div>
+
+  );
+};
+
+export default Footer;
